cb4dac6ae657124d96139ef47584252faa2f83fe
Akhmedov29 <bahodirahmedov26@gmail.com>
2025-02-15 13:59:33 +0500

    o'zgartirildi

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
       <div class="card">
         <div class="card-image">
           <img src=${meal.strMealThumb}>
-          <span class="card-title" style="color:aqua;">${meal.strMeal}</span>
+          <span class="card-title" style="color:aqua;"></span>
         </div>
         <div class="card-content">
-          <p>${meal.strInstructions.slice(0,20)}...</p>
+          <p style="width:200px; height:60px;">${meal.strMeal}</p>
         </div>
         <div class="card-action">
           <a href="${meal.strYoutube}" target="_blank">Watch reciept</a>
